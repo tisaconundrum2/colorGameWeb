784750a2dfd134dac35aac87aa356049da1a7536
tisaconundrum <ngf4@nau.edu>
2017-06-10 20:31:38 -0700

    Initial commit

---
+++ colorGame.js
@@ -0,0 +1,65 @@
+/**
+ * Created by tisaconundrum on 6/6/2017.
+ */
+var colors = getRandomColors(6);
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+
+for (var i = 0; i < squares.length; i++) {
+    // add initial color to squares
+    squares[i].style.backgroundColor = colors[i];
+    // add click listeners to squares
+    squares[i].addEventListener("click", function () {
+        //grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        // compare color of pickedColor
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct";
+            changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor
+        } else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
+        }
+    });
+}
+
+function changeColors(color) {
+    // loop through all squares
+    for (var i = 0; i < squares.length; i++) {
+        // change everything to given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function getRandomColors(num) {
+    // return an array of "num" elements
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        // get a random color val
+        // push into arr
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor() {
+    // pick "red" from 0 - 255
+    r = Math.floor(Math.random() * 256)
+    // pick "green" from 0 - 255
+    g = Math.floor(Math.random() * 256)
+    // pick "blue" from 0 - 255
+    b = Math.floor(Math.random() * 256)
+    return "rgb(" + r + ", " + g + ", " + b + ")"
+
+}
